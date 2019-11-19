b7a95895e30666894f51276168ac6b8dc01928bf
Antonio Busson <busson@outlook.com>
2019-11-19 18:58:16 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -28,6 +28,6 @@ class Config(object):
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = 1
-    MAIL_USERNAME = "ajgbusson@gmail.com"
-    MAIL_PASSWORD = "grandbus23xxggl"
+    MAIL_USERNAME = "<e-mail>"
+    MAIL_PASSWORD = "<password>"
   
